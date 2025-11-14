261ad77624a0be406625a228949858cfd9d91907
christian <christian.carvalho.adv@gmail.com>
2025-11-14 20:05:07 -0300

    Fix(compilation): Corrige importação default do LoginForm

--- src/app/components/auth/LoginForm.tsx
+++ src/app/components/auth/LoginForm.tsx
@@ -1,5 +1,5 @@
 // src/app/(auth)/login/page.tsx
-import { LoginForm } from '@/components/auth/LoginForm';
+import LoginForm from '@/components/auth/LoginForm'; // CORREÇÃO: Removido as chaves
 import { Metadata } from 'next';
 
 /**
